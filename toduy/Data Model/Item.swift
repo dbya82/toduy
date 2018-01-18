9fb2c29ab0da12d7c842760cd4dff543b44c769d
alvin <alvin@alvins-imac>
2018-01-18 00:10:02 -0800

    convert to MVC design pattern

---
+++ toduy/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  toduy
+//
+//  Created by alvin on 1/17/18.
+//  Copyright © 2018 alvin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
